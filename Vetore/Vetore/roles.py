895b7d71066441bfbafd801173c9a55169daa611
VettorePromotora <109700295+VettorePromotora@users.noreply.github.com>
2022-12-11 22:45:06 -0300

    Delete Vetore Directory

--- Vetore/Vetore/roles.py
+++
@@ -1,9 +0,0 @@
-from rolepermissions.roles import AbstractUserRole
-
-
-class AdministradorDaPlataforma(AbstractUserRole):
-    available_permissions = {'aceitar_requisicao': True, 'negar_requisicao': False}
-
-
-class UsuariosDaPlataforma(AbstractUserRole):
-    available_permissions = {'editar_requisicao': True, 'excluir_requisicao': False}
